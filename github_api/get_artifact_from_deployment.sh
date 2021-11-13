5705667b30cb74da819a01efbd7ae4e5be82a1f6
Andrew Boyd <8363252+bryopsida@users.noreply.github.com>
2021-11-12 20:11:30 -0600

    add error checks

--- github_api/get_artifact_from_deployment.sh
+++ github_api/get_artifact_from_deployment.sh
@@ -6,9 +6,16 @@ artifact_name="${3}"
 deploy_obj=$(curl -s -H "Authorization: token $GITHUB_PAT" "$deploy_url")
 sha=$(echo "$deploy_obj" | jq -r '.sha')
 workflow_run=$(curl -s -H "Authorization: token $GITHUB_PAT" "https://api.github.com/repos/bryopsida/actions-playground/actions/workflows/$artifact_workflow_name/runs")
-artifacts_url=$(echo $workflow_run | jq '.workflow_runs[] | select(.head_sha == "'$sha'") | .artifacts_url' | tr -d '"')
+artifacts_url=$(echo $workflow_run | jq 'first(.workflow_runs[] | select(.head_sha == "'$sha'") ) | .artifacts_url' | tr -d '"')
+if [ -z "$artifacts_url" ]; then
+  echo "No artifacts found"
+  exit 1
+fi
 artifacts=$(curl -s -H "Authorization: token $GITHUB_PAT" "$artifacts_url")
 archive_download_url=$(echo $artifacts | jq '.artifacts[] | select(.name == "'$artifact_name'") | .archive_download_url' | tr -d '"')
-echo "artifact download url = $archive_download_url "
-curl -O -J -L -H "Authorization: token $GITHUB_PAT" "$archive_download_url"
+if [ -z "$archive_download_url" ]; then
+  echo "No artifact found"
+  exit 1
+fi
+curl -s -O -J -L -H "Authorization: token $GITHUB_PAT" "$archive_download_url"
 unzip "$artifact_name".zip
