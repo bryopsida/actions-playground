11a63455038ca916874e1871470ad647db3860ae
Andrew Boyd <8363252+bryopsida@users.noreply.github.com>
2021-11-13 09:01:32 -0600

    add npm test

--- test_app/index.spec.js
+++ test_app/index.spec.js
@@ -1,10 +1,12 @@
 const assert = require('assert');
 const axios = require('axios');
 
+const port = process.env.APP_PORT || 3000;
+
 describe('TestApp', function() {
   describe('echo', function() {
     it('should return same message sent as an echo', function() {
-        return axios.get('http://localhost:3000?message=test').then(function(response) {
+        return axios.get(`http://localhost:${port}?message=test`).then(function(response) {
             assert.equal(response.data.message, 'test');
         });
     });
