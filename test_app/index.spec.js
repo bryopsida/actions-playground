c2efcc313f0d65c0e057635d7a8fd70fdcaf4cee
Andrew Boyd <8363252+bryopsida@users.noreply.github.com>
2021-11-13 11:04:09 -0600

    use hostname

--- test_app/index.spec.js
+++ test_app/index.spec.js
@@ -2,11 +2,12 @@ const assert = require('assert');
 const axios = require('axios');
 
 const port = process.env.APP_PORT || 3000;
+const hostname = process.env.APP_HOST || 'localhost';
 
 describe('TestApp', function() {
   describe('echo', function() {
     it('should return same message sent as an echo', function() {
-        return axios.get(`http://localhost:${port}?message=test`).then(function(response) {
+        return axios.get(`http://${hostname}:${port}?message=test`).then(function(response) {
             assert.equal(response.data.message, 'test');
         });
     });
